5b57f68423c762df9ca796c36634cf0c0794e22f
Mohammed Arabiat <tanjoodo@gmail.com>
2015-10-08 17:31:38 +0300

    removed unused usings

--- AutoProxy/Poller.cs
+++ AutoProxy/Poller.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 
 using System.Runtime.InteropServices;
 using Microsoft.Win32;
-using System.Windows;
 using System.Timers;
 using System.Threading;
 using NativeWifi;
