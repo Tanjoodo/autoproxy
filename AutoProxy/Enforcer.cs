249ee1232267b61bcc661fbfa0aaeb247288bafb
Mohammed Arabiat <tanjoodo@gmail.com>
2015-10-29 15:14:06 +0300

    NOT BUILDING added poller and started enforcer

---
+++ AutoProxy/Enforcer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NativeWifi;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace AutoProxy
+{
+    static class Enforcer
+    {
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+        static bool settingsReturn, refreshReturn;
+        static RegistryKey RegKey = Registry.CurrentUser.
+                OpenSubKey(@"Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+
+        public void Init()
+        {
+
+        }
+    }
+}
--- AutoProxy/Poller.cs
+++ AutoProxy/Poller.cs
@@ -2,142 +2,75 @@
 using System.Text;
 using System.Collections.ObjectModel;
 
-using System.Runtime.InteropServices;
-using Microsoft.Win32;
 using System.Timers;
-using System.Threading;
 using NativeWifi;
 namespace AutoProxy
 {
-    class Poller
+    static class Poller
     {
-        [DllImport("wininet.dll")]
-        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
-        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
-        public const int INTERNET_OPTION_REFRESH = 37;
-        bool settingsReturn, refreshReturn;
-        RegistryKey RegKey = Registry.CurrentUser.
-                OpenSubKey(@"Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
-        //TODO: Pair each poller with an iterface
-        
-        private ObservableCollection<ProxyRule> _rules;
-        private string _ssid = "";        
-        private string _interface = null;
+       
 
-        private Object l = new Object();
-        public Poller(ref ObservableCollection<ProxyRule>  Rules, string Interface)
+        static WlanClient.WlanInterface _wlanInterface = null;
+        static string _ssid = "";
+
+        public static void SetInterface(WlanClient.WlanInterface wlanInterface)
         {
-            _rules = Rules;
-            _rules.CollectionChanged += RulesChanged;
-            _interface = Interface;
-        }
-        public void RulesChanged(Object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            _rules = (ObservableCollection<ProxyRule>)sender;
-        }
-        public void StartPolling()
-        {
-            var poll_timer = new System.Timers.Timer(100);
-            poll_timer.Elapsed += TimerEvent;
-            poll_timer.Start();         
-        }
-        public void TimerEvent(Object source, ElapsedEventArgs e)
-        {
-            if (_interface == null)
-                return;
-            var thread = new Thread(this.Poll); 
-            thread.Start();
-            thread.Join();
-        }
-        private void Poll()
-        {
-            if (Monitor.TryEnter(l)) //Allow only one thread to enter this section at a time, should be better than lock() because as far as I understand it,
-                                     //lock will queue all the threads, and that would cause problems.
+            if (_wlanInterface == null)
+                _wlanInterface = wlanInterface;
+            else
             {
-                UpdateSSID();
-
-
-                var rule = FindRule();
-                if (rule != null)
-                {
-                    if ((string)RegKey.GetValue("ProxyServer") != rule.Proxy.Host || (int)RegKey.GetValue("ProxyEnable") != 1)
-                        ChangeProxy(rule.Proxy);
-                }
-                else
-                {
-                    if ((int)RegKey.GetValue("ProxyEnable") != 0)
-                        DisableProxy();
-                }
-
-                Monitor.Exit(l);
+                StopPolling(); //detach Poll() from old interface
+                _wlanInterface = wlanInterface;
+                StartPolling();
             }
+        }
+
+        public static void StartPolling()
+        {
+            if (_wlanInterface != null)
+                _wlanInterface.WlanConnectionNotification += Poll;
             else
-                return;
+                throw new NullReferenceException();
         }
 
-        private ProxyRule FindRule()
+        public static void StopPolling()
         {
-            foreach (var y in _rules)
-                if (y.SSID == _ssid)
-                    return y;
-            return null;
-        }
-
-        void UpdateSettings()
-        {
-            settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
-            refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
-        }
-
-        void ChangeProxy(ProxyHost host)
-        {
-            RegKey.SetValue("ProxyEnable", 1);
-            RegKey.SetValue("ProxyServer", host.Host);
-            UpdateSettings();
-            if (_ssid == null || _ssid == "")
-                MainWindow.tb_icon.ShowBalloonTip("Disconnected from Wifi", "Changed proxy to: " + host.Host, Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info);
+            if (_wlanInterface != null)
+                _wlanInterface.WlanConnectionNotification -= Poll;
             else
-                MainWindow.tb_icon.ShowBalloonTip("Now connected to: " + _ssid, "Changed proxy to: " + host.Host, Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info);
-
+                throw new NullReferenceException("Wlan interface was not set");
         }
 
-        void DisableProxy()
+        static void Poll(Wlan.WlanNotificationData notifyData, Wlan.WlanConnectionNotificationData connNotifyData)
         {
-            RegKey.SetValue("ProxyEnable", 0);
-            UpdateSettings();
-            if (_ssid == null || _ssid == "")            
-                MainWindow.tb_icon.ShowBalloonTip("Disconnected from Wifi", "Proxy disabled", Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info);            
-            else
-                MainWindow.tb_icon.ShowBalloonTip("Now connected to: " + _ssid, "Proxy disabled.", Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info);
+            Poll();
         }
 
-        void UpdateSSID()
+        public static void Poll()
         {
-            WlanClient wlan = null;
             try
             {
-                wlan = new WlanClient();
+                _ssid = new String
+                    (
+                        ASCIIEncoding.ASCII.GetChars
+                        (
+                            _wlanInterface.CurrentConnection.wlanAssociationAttributes.dot11Ssid.SSID,
+                            0,
+                            (int)_wlanInterface.CurrentConnection.wlanAssociationAttributes.dot11Ssid.SSIDLength
+                        )
+                    );
             }
-            catch (Exception)
+            catch
             {
-                return;
-            }
-            foreach (var inter in wlan.Interfaces)
-            {
-                if (inter.InterfaceDescription == _interface)
-                {
-                    try
-                    {
-                        _ssid = new String(Encoding.ASCII.GetChars(inter.CurrentConnection.wlanAssociationAttributes.dot11Ssid.SSID, 0,
-                            (int)inter.CurrentConnection.wlanAssociationAttributes.dot11Ssid.SSIDLength));
-                    }
-                    catch (Exception)
-                    {
-                        _ssid = null; //not connected to a network
-                    }
-                    return;
-                }
+                _ssid = "";
             }
         }
+
+        public static string GetSsid()
+        {
+            Poll();
+            return _ssid;
+        }
+
     }
 }
